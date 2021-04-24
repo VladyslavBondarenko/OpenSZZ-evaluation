a67682c6678bff5b01e4ba27f1a639a19f556b6d
Vladyslav Bondarenko <vladyslav.bondarenko@ut.ee>
2021-04-25 01:17:06 +0300

    fix 'no such file or directory'

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const fs = require("fs");
 const formidable = require("formidable");
 
 const { getResult } = require("./app.helpers.js");
@@ -13,18 +12,7 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.post("/compare", async (req, res) => {
   const form = new formidable.IncomingForm();
   form.parse(req, async (err, fields, files) => {
-    const openszzFileName = "./files/openszz.csv";
-    const benchmarkFileName = "./files/benchmark.csv";
-    const openszzFile = files.openszz.path;
-    const benchmarkFile = files.benchmark.path;
-    const onError = (err) => {
-      if (err) throw err;
-    };
-
-    fs.rename(openszzFile, "./files/openszz.csv", onError);
-    fs.rename(benchmarkFile, "./files/benchmark.csv", onError);
-
-    const result = await getResult(openszzFileName, benchmarkFileName);
+    const result = await getResult(files.openszz.path, files.benchmark.path);
 
     res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(result));
